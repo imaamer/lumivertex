c498591a7afa7822e17845d7e91998e93523eb44
Muhammad Amir <amir@192.168.1.30>
2026-03-01 03:40:26 +0500

    coming soon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import { Header } from "@/components/Header";
+import Footer from "@/components/Footer";
 import "./globals.css";
 import ComingSoon from "@/components/ComingSoon";
 
@@ -15,7 +16,9 @@ export const metadata: Metadata = {
     "Lumivertex converges AI technology, data-driven marketing strategy, and intelligent lead generation to architect measurable business growth. Free strategic consultation.",
 };
 
-export default function RootLayout({}: Readonly<{
+export default function RootLayout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
@@ -24,6 +27,9 @@ export default function RootLayout({}: Readonly<{
         className={`${inter.variable} font-sans flex min-h-screen flex-col antialiased`}
         suppressHydrationWarning
       >
+        {/* <Header />
+        <main className="flex-1">{children}</main>
+        <Footer /> */}
         <ComingSoon />
       </body>
     </html>
