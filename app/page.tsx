893dacf6d4fc12990e1d28f0a6fceb12b5b77359
Muhammad Amir <amir@192.168.1.22>
2026-03-22 03:51:54 +0500

    fav icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturesSection from "@/components/FeaturesSection";
 
-
-
 export default function Home() {
   return (
     <div className="bg-background">
